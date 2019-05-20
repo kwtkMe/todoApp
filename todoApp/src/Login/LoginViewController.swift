30b123e56c4224d44631506b02a3dfb892568b32
mychair.kke@gmail.com <mychair.kke@gmail.com>
2019-05-20 20:19:52 +0900

    edit

--- todoApp/src/Login/LoginViewController.swift
+++ todoApp/src/Login/LoginViewController.swift
@@ -7,27 +7,10 @@
 //
 
 import UIKit
-import FirebaseAuth
+import Firebase
+import FirebaseUI
 import TwitterKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: FUIAuthPickerViewController {
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
-            if let session = session {
-                let authToken = session.authToken
-                let authTokenSecret = session.authTokenSecret
-                let credential = TwitterAuthProvider.credential(withToken: session.authToken, secret: session.authTokenSecret)
-                
-                Auth.auth().signIn(with: credential) { (authResult, error) in
-                    if let error = error { return }
-                    //Sign In Completed
-                }
-            }
-        })
-        logInButton.center = self.view.center
-        self.view.addSubview(logInButton)
-    }
 }
